f5752f552da0bc1ac49ac525d4f7320a84b98be3
KlarkDeveloper <143858729+KlarkDeveloper@users.noreply.github.com>
2023-09-02 21:20:42 -0600

    Raycast Recoger Objetos

--- Assets/Scripts/Pickable.cs
+++ Assets/Scripts/Pickable.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class Pickable : Interactable
 {
+
+    public Transform zoneArm;
+    public Rigidbody rb;
+    public Collider coll;
+
+    private void Start()
+    {
+        
+    }
+
     public override void Interact()
     {
         base.Interact();
 
-        Destroy(gameObject);
+        transform.SetParent(zoneArm);
+        transform.position = new Vector3(0,0,0);
+        transform.rotation = new Quaternion(0,0,0,0);
+        transform.position = zoneArm.position;
+        
+        rb.isKinematic = true; coll.enabled = false; rb.useGravity = false;
+
+
     }
 
 }
