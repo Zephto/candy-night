92fa1a790b9a40ced95f9472399a94c29c87e9ac
KlarkDeveloper <143858729+KlarkDeveloper@users.noreply.github.com>
2023-09-05 17:12:05 -0600

    Texturas Agregadas

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    private Transform Cam;
+    public Transform Cam;
 
     public float rayDistance;
 
@@ -16,7 +16,7 @@ public class Door : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        Cam = Camera.main.transform;
+        
     }
 
     // Update is called once per frame
