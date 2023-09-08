3c99eddb28f544f60b7de809d1bb6f6e63a1e65a
hugo-gonzalez <hugo.gonzalez@binmatter.com>
2023-09-07 23:44:07 -0600

    Fix PS1 raw texture dimensions

---
+++ Assets/Scripts/Enviroment/LightsEmergencyBehaviour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Componente usado para controlar las luces
+/// </summary>
+public class LightsEmergencyBehaviour : MonoBehaviour {
+	#region Public references
+	[SerializeField] private Material normalLights;
+	[SerializeField] private Material damageLights;
+	// [SerializeField] private GameObject lightReference;
+	#endregion
+	
+	#region private references
+	private SpatialSound spatialSoundRef;
+	private MeshRenderer meshRenderer;
+	#endregion
+
+	void Awake() {
+		spatialSoundRef = this.GetComponent<SpatialSound>();
+		meshRenderer = this.GetComponent<MeshRenderer>();
+	}
+
+	#region Public Methods
+	public void TurnOn(){
+		spatialSoundRef.enabled = true;
+		meshRenderer.material = normalLights;
+		// lightReference.SetActive(true);
+	}
+
+	public void TurnOff(){
+		spatialSoundRef.enabled = false;
+		meshRenderer.material = damageLights;
+		// lightReference.SetActive(false);
+	}
+	#endregion
+}
